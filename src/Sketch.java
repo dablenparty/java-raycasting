e9672334b4cbb137dfb24205988cbaf32b09de43
dablenparty <hdavenport8902@gmail.com>
2022-06-13 19:17:24 -0400

    Created Sketch class for Processing

---
+++ src/Sketch.java
@@ -0,0 +1,13 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    @Override
+    public void settings() {
+        size(400, 400);
+    }
+
+    public Sketch run() {
+        runSketch();
+        return this;
+    }
+}
