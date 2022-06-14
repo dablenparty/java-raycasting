81661a41369cf78f742a056d1fb1b34a9652d0cf
dablenparty <hdavenport8902@gmail.com>
2022-06-13 21:11:52 -0400

    Converted Line to a class to allow mutable start/end

--- src/Line.java
+++ src/Line.java
@@ -1,8 +1,56 @@
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public record Line(PVector start, PVector end) {
+import java.util.Objects;
+
+public final class Line {
+    private PVector start;
+    private PVector end;
+
+    public Line(PVector start, PVector end) {
+        this.start = start;
+        this.end = end;
+    }
+
     public void draw(PApplet app) {
         app.line(start.x, start.y, end.x, end.y);
     }
+
+    public PVector getStart() {
+        return start;
+    }
+
+    public void setStart(PVector start) {
+        this.start = start;
+    }
+
+    public PVector getEnd() {
+        return end;
+    }
+
+    public void setEnd(PVector end) {
+        this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Line) obj;
+        return Objects.equals(this.start, that.start) &&
+                Objects.equals(this.end, that.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return "Line[" +
+                "start=" + start + ", " +
+                "end=" + end + ']';
+    }
+
 }
